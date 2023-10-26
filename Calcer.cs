d76d47ad8d6d0081f57cb98dac7c6dd6a3cc84bf
Nikita <Nikita@DESKTOP-BPFJRUA>
2023-10-26 22:47:27 +0700

    Добавьте файлы проекта.

---
+++ Calcer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    internal class Calcer
+    {
+        private Stack<double> numbers = new Stack<double>();
+        private Stack<char> operators = new Stack<char>();
+
+        private Dictionary<char, int> priority = new Dictionary<char, int>()
+        {
+            {'(','0'},
+            {'+','1'},
+            {'-','1'},
+            {'*','2'},
+            {'/','2'},
+            {'~','3'},
+        };
+
+        private double GetNumberFromString(string str, ref int ind)
+        {
+            string num = "";
+
+            while (ind < str.Length)
+            {
+                char c = str[ind];
+                if (Char.IsDigit(c) || c == ',')
+                {
+                    num += c;
+                    ind++;
+                }
+                else
+                {
+                    ind--;
+                    break;
+                }
+            }
+            return Double.Parse(num);
+        }
+
+        private double evaluate(double b, double a, char oper)
+        {
+            switch (oper)
+            {
+                case '+': return a + b;
+                case '-': return a - b;
+                case '*': return a * b;
+                case '/': return a / b;
+                default:  throw new Exception("Ты дебил блять, ты как сломал то что нельзя сломать ебанько");
+            }
+        }
+
+        public string SolveExpression(string expression)
+        {
+            for (int pos = 0; pos < expression.Length; pos++)
+            {
+                char c = expression[pos];
+                if (Char.IsDigit(c))
+                {
+                    numbers.Push(GetNumberFromString(expression, ref pos));
+                }
+                else if (c == '(')
+                {
+                    operators.Push('(');
+                }
+                else if (c == ')')
+                {
+                    while (operators.Peek() != '(')
+                    {
+                        if (priority[operators.Peek()] == '~')
+                        {
+                            numbers.Push(-numbers.Pop());
+                        }
+                        else
+                        {
+                            numbers.Push(evaluate(numbers.Pop(), numbers.Pop(), operators.Pop()));
+                        }
+                    }
+                    operators.Pop();
+                }
+                else if (c == '-' && (pos == 0 || expression[pos-1] == '('))
+                {
+                    operators.Push('~');
+                }
+                else 
+                {
+                    while (operators.Count > 0 && priority[operators.Peek()] > priority[c])
+                    {
+                        if (operators.Peek() == '~')
+                        {
+                            numbers.Push(-numbers.Pop());
+                            operators.Pop();
+                        }
+                        else
+                        {
+                           numbers.Push(evaluate(numbers.Pop(), numbers.Pop(), operators.Pop()));
+                        }
+                       
+                    }
+                    operators.Push(c);
+                }
+
+            }
+
+            while (operators.Count > 0)
+            {
+                if (priority[operators.Peek()] == '~')
+                {
+                    numbers.Push(-numbers.Pop());
+                }
+                else
+                {
+                    numbers.Push(evaluate(numbers.Pop(), numbers.Pop(), operators.Pop()));
+                }
+            }
+
+            return numbers.Pop().ToString();
+        }
+
+        public double test()
+        {
+            double k = 0;
+            return 1.0 / k;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulator
+{
+
+    public partial class MainWindow : Window
+    {
+        Calcer forCalc = new Calcer();
+        private const string DefaultExpression = "Введите выражение...";
+        private const string ErrorExpression = "Айайай нильзя на нуль делить";
+        public MainWindow()
+        {
+            forCalc.test();
+            InitializeComponent();
+        }
+
+        private void Number_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox.Text == DefaultExpression || TextBox.Text == ErrorExpression)
+            {
+                TextBox.Text = "";
+            }
+
+            TextBox.Text += (sender as Button).Content;
+        }
+
+        private void Operator_button_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt = sender as Button;
+
+            if (bt.Content.ToString()[0] == '-' || bt.Content.ToString()[0] == '(')
+            {
+                if (TextBox.Text == DefaultExpression) TextBox.Text = bt.Content.ToString();
+                
+            }
+
+            if (TextBox.Text == DefaultExpression || TextBox.Text.Last() == '+' || TextBox.Text.Last() == '-' || TextBox.Text.Last() == '*' || TextBox.Text.Last() == '/')
+            {
+                return;
+            }
+
+            TextBox.Text += (sender as Button).Content;
+        }
+
+        private void Delete_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox.Text.Length > 0 && TextBox.Text != DefaultExpression)
+            {
+                TextBox.Text = TextBox.Text.Substring(0, TextBox.Text.Length - 1);
+            }
+
+            if (TextBox.Text.Length == 0)
+            {
+                TextBox.Text = DefaultExpression;
+            }
+        }
+
+        private void Clear_button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = DefaultExpression;
+            
+        }
+
+        private void Equal_button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (TextBox.Text != DefaultExpression)
+            { 
+                var solve = forCalc.SolveExpression(TextBox.Text);
+                TextBox.Text = solve;
+            }
+        }
+    }
+}
